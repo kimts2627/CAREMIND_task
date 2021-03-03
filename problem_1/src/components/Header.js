bda377d4a6840a141ed23f807bd17aa3ecb26023
kimts2627 <kimtstststs@gmail.com>
2021-03-03 22:28:42 +0900

    Problem1 Add Header function

--- problem_1/src/components/Header.js
+++ problem_1/src/components/Header.js
@@ -6,13 +6,13 @@ const Header = () => {
 
   return(
     <header className='header-container'>
-      <a href='#home' className='header-logo'>Logo</a>
-      <div className='header-menu'>
-        <a href='#news' className='header-menus'>news</a>
-        <a href='#contact' className='header-menus'>contact</a>
-        <a href='#about' className='header-menus'>about</a>
+      <a href='#home' className='header-logo'><p>Logo</p></a>
+      <div className={ isMenuOpen ? 'header-menu' : 'header-menu-none'}>
+        <a href='#news' className='header-menus'><p>News</p></a>
+        <a href='#contact' className='header-menus'><p>Contact</p></a>
+        <a href='#about' className='header-menus'><p>About</p></a>
       </div>
-      <div className='header-menu-button' onClick={undefined}>
+      <div className='header-menu-button' onClick={() => setMenuOpen(!isMenuOpen)}>
         <i className='fa fa-bars'></i>
       </div>
     </header>
