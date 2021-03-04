2a2e3df1dc3b2e78a021d0339fb6f864db539966
kimts2627 <kimtstststs@gmail.com>
2021-03-04 13:29:29 +0900

    Problem2-2 Complete

--- problem_2-2/src/App.js
+++ problem_2-2/src/App.js
@@ -1,8 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import Chart from "react-google-charts";
 import './App.css';
 
+
 function App() {
+
   return(
-    <div>hello world</div>
+    <Chart
+      width={900}
+      height={500}
+      chartType='PieChart'
+      loader={<div>Loading Chart</div>}
+      data={[
+        ['Task', 'Hours per Day'],
+        ['😃', 11],
+        ['시간내어', 2],
+        ['검토', 2],
+        ['해 주셔서', 2],
+        ['감사드립니다', 7]
+      ]}
+      options={{
+        title: '주니어 프론트엔드 개발자 김태수',
+        is3D: true
+      }}
+      rootProps={{ 'data-testid': '2' }}
+    />
   );
 }
 
